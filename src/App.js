94096b57b254b9e1a633616d433442346c8f233c
jessieward <jessica_ward25@milton.edu>
2024-11-12 12:24:35 -0500

    tictactoe

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Component from './component';
 
 function App() {
   return (
@@ -7,15 +8,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Jessie Learning React.
         </p>
+        <Component/>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I am excited!
         </a>
       </header>
     </div>
---
+++ src/component.js
@@ -0,0 +1,7 @@
+function Component(){
+    return(
+        <p> I am a component</p>
+
+    );
+}
+export default Component;
