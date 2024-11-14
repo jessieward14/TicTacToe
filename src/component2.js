cdc4e1e18af4e18039774de09d5d3354910d494a
jessieward <jessica_ward25@milton.edu>
2024-11-14 09:16:44 -0500

    favoritethings

---
+++ src/component2.js
@@ -0,0 +1,13 @@
+function Component2(){
+    return(
+        <ul>
+        <li>grapes</li>
+        <li>popcorn</li>
+        <li>sweetpotato</li>
+        <li>boursin cheese</li>
+        <li>tortillas</li>
+        <li>choc chip cookies</li>
+      </ul>
+    );
+}
+export default Component2;
